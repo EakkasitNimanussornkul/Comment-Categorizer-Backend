f1c164e545b00038c02aa68e8af501465f022d8e
Eakkasit Nimanussornkul <eakkasit_nim@cmu.ac.th>
2026-02-19 21:07:04 +0700

    update schema to have app_id to be able to save to database

--- app/schemas.py
+++ app/schemas.py
@@ -8,12 +8,14 @@ class ReviewRecieve(BaseModel):
     #ge stands for "greater than or equal to", le stands for "less than or equal to"
     # This means the rating must be between 1 and 5 (inclusive)
     rating: int = Field(..., ge=1, le=5, description="Star rating from 1 to 5")
+    app_id: int = Field(None, description="ID of the app being reviewed")
 
 
 class ReviewResponse(BaseModel):
     text: str
     rating: int
     sentiment: str  # e.g., "Bug", "Feature", "Noise"
+    app_id: int
     
     # This config line is for pydantic to read a sql object instead of a dict
     class Config:

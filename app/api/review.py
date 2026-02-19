60c839c731c10dd3328e5ec51c1deb7526f79c2a
Eakkasit Nimanussornkul <eakkasit_nim@cmu.ac.th>
2026-02-19 14:36:57 +0700

    added /predict for predicting review

--- app/api/review.py
+++ app/api/review.py
@@ -1,1 +1,15 @@
 # Handles /submit-review and /get-reviews endpoints
+from fastapi import APIRouter
+from app.core.services.ai_predict import predictor
+from app.schemas import ReviewRecieve, ReviewResponse
+
+router = APIRouter()
+
+@router.post("/predict", response_model=ReviewResponse)
+def predict_review(review: ReviewRecieve):
+    prediction_result = predictor.predict_review_category(review.text, review.rating)
+    return {
+        "text": review.text,
+        "rating": review.rating,
+        "sentiment": prediction_result
+    }

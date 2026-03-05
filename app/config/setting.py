59e58f2e77f5183a2c7fc3cb208c72823a0a8a70
Eakkasit Nimanussornkul <eakkasit_nim@cmu.ac.th>
2026-03-05 16:35:04 +0700

    added the supabase url and key for user auth inside env and read in setting.py

--- app/config/setting.py
+++ app/config/setting.py
@@ -3,9 +3,11 @@ from pydantic import SecretStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    DATABASE_URL: SecretStr 
+    DATABASE_URL: SecretStr
+    
+    SUPABASE_URL: str
+    SUPABASE_KEY: str  
 
-    # Modern Pydantic v2 way to tell it to read the .env file
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 # Create the globally available settings object

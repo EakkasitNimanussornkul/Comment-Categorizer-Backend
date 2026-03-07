0d48a11d862efc4dd403facb9ca1fd74449194ad
Eakkasit Nimanussornkul <eakkasit_nim@cmu.ac.th>
2026-03-07 12:48:35 +0700

    change schemas for app to return review_count as well

--- app/schemas.py
+++ app/schemas.py
@@ -1,7 +1,10 @@
 # Defines Pydantic models (Data validation for inputs/outputs)
-from pydantic import BaseModel, Field
+from uuid import UUID
+
+from pydantic import BaseModel, ConfigDict, Field
 from typing import Optional
 
+# for reveiw
 class ReviewRecieve(BaseModel):
     # description is for /docs
     text: str = Field(..., min_length=1, max_length=1000, description="The review content")
@@ -9,7 +12,6 @@ class ReviewRecieve(BaseModel):
     # This means the rating must be between 1 and 5 (inclusive)
     rating: int = Field(..., ge=1, le=5, description="Star rating from 1 to 5")
     app_id: int = Field(None, description="ID of the app being reviewed")
-
 
 class ReviewResponse(BaseModel):
     text: str
@@ -21,6 +23,8 @@ class ReviewResponse(BaseModel):
     class Config:
         from_attributes = True
 
+
+# for auth
 class RegisterRequest(BaseModel):
     email: str
     password: str
@@ -29,3 +33,19 @@ class RegisterRequest(BaseModel):
 class LoginRequest(BaseModel):
     email: str
     password: str
+
+
+# for apps
+class AppCreate(BaseModel):
+    name: str
+    description: Optional[str] = None
+
+class AppResponse(BaseModel):
+    id: int
+    name: str
+    description: Optional[str] = None
+    user_id: UUID
+    review_count: int
+
+    # This is the magic line that lets Pydantic read SQLAlchemy database models
+    model_config = ConfigDict(from_attributes=True)
